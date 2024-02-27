352c7ff9638aa597a50dd84e4f114778d8fe21d0
Riccardo Mazzarini <me@noib3.dev>
2024-02-27 17:52:28 +0100

    collab: test the default `CollabConfig::server_address`

--- crates/collab/src/config.rs
+++ crates/collab/src/config.rs
@@ -22,8 +22,33 @@ impl Default for CollabConfig {
         Self {
             // TODO: this should be a path relative to the `/nomad` path.
             project_dir: PathBuf::new(),
-            server_address: Url::parse("collab.nomad.foo").unwrap(),
+            server_address: Url::parse("tcp://collab.nomad.foo").unwrap(),
             server_port: 64420,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Tests that the default `CollabConfig` can be created without panicking.
+    #[test]
+    fn collab_config_default() {
+        let _config = CollabConfig::default();
+    }
+
+    /// Tests that the server address of the default `CollabConfig` can be
+    /// resolved to a valid `SocketAddr`.
+    #[test]
+    fn collab_config_resolve_server_addr() {
+        let config = CollabConfig::default();
+
+        let addrs = config
+            .server_address
+            .socket_addrs(|| Some(config.server_port))
+            .unwrap();
+
+        assert_eq!(addrs.len(), 1);
     }
 }
