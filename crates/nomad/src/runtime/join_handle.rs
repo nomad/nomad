14fc502805f7b12099f6ed2a6b8f4e65c67ec0e2
Riccardo Mazzarini <me@noib3.dev>
2024-03-02 00:41:34 +0100

    nomad(runtime): implement `Future` for `JoinHandle`

--- crates/nomad/src/runtime/join_handle.rs
+++ crates/nomad/src/runtime/join_handle.rs
@@ -2,16 +2,30 @@ use core::future::Future;
 use core::pin::Pin;
 use core::task::{Context, Poll};
 
-/// TODO: docs
-pub struct JoinHandle<T> {
-    _p: core::marker::PhantomData<T>,
+use async_task::Task;
+use pin_project_lite::pin_project;
+
+pin_project! {
+    /// TODO: docs
+    pub struct JoinHandle<T> {
+        #[pin]
+        task: Task<T>,
+    }
+}
+
+impl<T> JoinHandle<T> {
+    /// TODO: docs
+    #[inline]
+    pub(crate) fn new(task: Task<T>) -> JoinHandle<T> {
+        JoinHandle { task }
+    }
 }
 
 impl<T> Future for JoinHandle<T> {
     type Output = T;
 
     #[inline]
-    fn poll(self: Pin<&mut Self>, _ctx: &mut Context<'_>) -> Poll<T> {
-        todo!();
+    fn poll(self: Pin<&mut Self>, ctx: &mut Context<'_>) -> Poll<T> {
+        self.project().task.poll(ctx)
     }
 }
