b09710192acd6ef4cc8df3592398807b160f1a56
Riccardo Mazzarini <me@noib3.dev>
2024-03-03 16:06:56 +0100

    nomad: implement `panic_hook::init()`

--- crates/nomad/src/log/panic_hook.rs
+++ crates/nomad/src/log/panic_hook.rs
@@ -1,1 +1,25 @@
-pub(super) fn init() {}
+use std::panic;
+
+use super::error;
+
+/// Initializes the panic hook.
+pub(super) fn init() {
+    panic::set_hook(Box::new(|info| {
+        let Some(location) = info.location() else {
+            error!("{info}");
+            return;
+        };
+
+        error!(
+            "panicked at {file}:{line}:{col}{maybe_msg}",
+            file = location.file(),
+            line = location.line(),
+            col = location.column(),
+            maybe_msg = info
+                .payload()
+                .downcast_ref::<&str>()
+                .map(|msg| format!(": {msg}"))
+                .unwrap_or_default(),
+        );
+    }));
+}
