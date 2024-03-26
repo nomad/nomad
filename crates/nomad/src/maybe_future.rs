d3b769e5821e940fa55ab975b6c30bb91127e564
Riccardo Mazzarini <me@noib3.dev>
2024-03-26 17:15:46 +0100

    nomad: add `into_future()` and `into_ready()` on `MaybeFuture`

--- crates/nomad/src/maybe_future.rs
+++ crates/nomad/src/maybe_future.rs
@@ -1,9 +1,13 @@
 //! TODO: docs
 
 use core::future::Future;
+use core::pin::{pin, Pin};
+use core::task::{Context, Poll};
+
+use pin_project::pin_project;
 
 /// TODO: docs
-pub trait MaybeFuture {
+pub trait MaybeFuture: Sized {
     /// TODO: docs
     type Output;
 
@@ -12,6 +16,30 @@ pub trait MaybeFuture {
 
     /// TODO: docs
     fn into_enum(self) -> MaybeFutureEnum<Self::Future, Self::Output>;
+
+    /// TODO: docs
+    #[inline]
+    fn into_future(self) -> impl Future<Output = Self::Output> {
+        match self.into_enum() {
+            MaybeFutureEnum::Ready(output) => {
+                MaybeFutureFuture::Ready(core::future::ready(output))
+            },
+
+            MaybeFutureEnum::Future(future) => {
+                MaybeFutureFuture::Future(future)
+            },
+        }
+    }
+
+    /// TODO: docs
+    #[track_caller]
+    #[inline]
+    fn into_ready(self) -> Self::Output {
+        match self.into_enum() {
+            MaybeFutureEnum::Ready(output) => output,
+            MaybeFutureEnum::Future(_) => panic!("future is not ready"),
+        }
+    }
 }
 
 /// TODO: docs
@@ -41,6 +69,24 @@ where
     #[inline]
     fn from(future: F) -> Self {
         MaybeFutureEnum::Future(future)
+    }
+}
+
+#[pin_project(project = MaybeFutureFutureProj)]
+enum MaybeFutureFuture<F: Future<Output = T>, T> {
+    Ready(core::future::Ready<T>),
+    Future(#[pin] F),
+}
+
+impl<F: Future<Output = T>, T> Future for MaybeFutureFuture<F, T> {
+    type Output = T;
+
+    #[inline]
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.project() {
+            MaybeFutureFutureProj::Ready(future) => Pin::new(future).poll(cx),
+            MaybeFutureFutureProj::Future(future) => future.poll(cx),
+        }
     }
 }
 
