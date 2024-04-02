7e5adb60dd9f67c03a4de2b8cba66ba21f49926a
Riccardo Mazzarini <me@noib3.dev>
2024-04-02 16:05:13 +0200

    nomad: refactor panic logging

--- crates/nomad/src/log/panic_hook.rs
+++ crates/nomad/src/log/panic_hook.rs
@@ -1,25 +1,49 @@
-use std::panic;
+use core::any::Any;
+use core::fmt::{self, Display};
+use std::panic::{self, Location, PanicInfo};
 
 use super::error;
 
 /// Initializes the panic hook.
 pub(super) fn init() {
-    panic::set_hook(Box::new(|info| {
-        let Some(location) = info.location() else {
-            error!("{info}");
-            return;
-        };
+    panic::set_hook(Box::new(|info| error!("{}", PanicMsg::from(info))));
+}
 
-        error!(
-            "panicked at {file}:{line}:{col}{maybe_msg}",
-            file = location.file(),
-            line = location.line(),
-            col = location.column(),
-            maybe_msg = info
-                .payload()
-                .downcast_ref::<&str>()
-                .map(|msg| format!(": {msg}"))
-                .unwrap_or_default(),
-        );
-    }));
+struct PanicMsg<'a> {
+    location: Option<&'a Location<'a>>,
+    msg: Option<&'a dyn Display>,
+}
+
+impl<'a> From<&'a PanicInfo<'_>> for PanicMsg<'a> {
+    fn from(info: &'a PanicInfo<'_>) -> Self {
+        let payload = info.payload();
+
+        let msg = downcast_display::<&str>(payload)
+            .or_else(|| downcast_display::<String>(payload))
+            .or_else(|| downcast_display::<&String>(payload));
+
+        Self { location: info.location(), msg }
+    }
+}
+
+impl Display for PanicMsg<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "panicked")?;
+
+        if let Some(loc) = &self.location {
+            write!(f, " at {}:{}:{}", loc.file(), loc.line(), loc.column())?;
+        }
+
+        if let Some(msg) = &self.msg {
+            write!(f, ": {}", msg)?;
+        }
+
+        Ok(())
+    }
+}
+
+fn downcast_display<T: Any + Display>(
+    value: &dyn Any,
+) -> Option<&dyn Display> {
+    value.downcast_ref::<T>().map(|msg| msg as &dyn Display)
 }
