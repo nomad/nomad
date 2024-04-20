05eb100bc596f3abb846b1581c186be39f2d90e2
Riccardo Mazzarini <me@noib3.dev>
2024-04-20 12:56:49 +0200

    nix: add `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs.lib) lists;
+
+        mkPkgs = isNightly: (import nixpkgs {
+          inherit system;
+          overlays = lists.optionals isNightly [
+            neovim-nightly-overlay.overlay
+          ];
+        });
+
+        mkShell = { nightly }: (
+          let
+            pkgs = mkPkgs nightly;
+            inherit (pkgs) lib stdenv;
+          in
+          pkgs.mkShell {
+            buildInputs = with pkgs; [
+            ] ++ lib.optional stdenv.isDarwin [
+              darwin.apple_sdk.frameworks.AppKit
+              libiconv
+            ];
+
+            packages = with pkgs; [
+              # neovim
+              pkg-config
+            ];
+          }
+        );
+      in
+      {
+        devShells = {
+          default = mkShell { nightly = false; };
+          nightly = mkShell { nightly = true; };
+        };
+      }
+    );
+}
