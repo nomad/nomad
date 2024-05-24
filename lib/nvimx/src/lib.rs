b3de8fcfd4ab06779ff58c04351143f29bb8dde5
Riccardo Mazzarini <me@noib3.dev>
2024-05-24 15:40:28 +0800

    nvimx: add `executor`

--- lib/nvimx/src/lib.rs
+++ lib/nvimx/src/lib.rs
@@ -1,1 +1,9 @@
+//! TODO: docs
 
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
+pub use nvim_oxi as oxi;
+
+#[cfg(feature = "executor")]
+#[cfg_attr(docsrs, doc(cfg(feature = "executor")))]
+pub mod executor;
