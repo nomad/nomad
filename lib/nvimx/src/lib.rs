7c5f42f3141d55ab7900351dd31293b71636cfe2
Riccardo Mazzarini <me@noib3.dev>
2024-05-24 18:17:22 +0800

    nvimx: add `nvimx-macros`

--- lib/nvimx/src/lib.rs
+++ lib/nvimx/src/lib.rs
@@ -6,6 +6,9 @@ pub use nvim_oxi as oxi;
 #[cfg(feature = "executor")]
 #[cfg_attr(docsrs, doc(cfg(feature = "executor")))]
 pub use nvimx_executor as executor;
+#[cfg(feature = "macros")]
+#[cfg_attr(docsrs, doc(cfg(feature = "macros")))]
+pub use nvimx_macros as macros;
 #[cfg(feature = "project")]
 #[cfg_attr(docsrs, doc(cfg(feature = "project")))]
 pub use nvimx_project as project;
