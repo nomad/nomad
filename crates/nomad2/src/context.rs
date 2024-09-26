4edf4e750525c3aba767b63457cdb09549990979
Riccardo Mazzarini <me@noib3.dev>
2024-09-26 11:19:49 +0800

    nomad2: implement `Context::subscribe()`

--- crates/nomad2/src/context.rs
+++ crates/nomad2/src/context.rs
@@ -1,6 +1,48 @@
+use core::any::{Any, TypeId};
+use std::collections::HashMap;
+use std::sync::atomic::{AtomicU32, Ordering};
+use std::sync::{Arc, Mutex};
+
+use crate::subscription::{self, AnyReceiver, InactiveReceiver};
+use crate::{Editor, Event, Subscription};
+
 /// TODO: docs.
 pub struct Context<E> {
-    editor: E,
+    inner: Arc<Mutex<ContextInner<E>>>,
+}
+
+impl<E: Editor> Context<E> {
+    /// TODO: docs.
+    #[inline]
+    pub fn subscribe<T>(&self, event: T) -> Subscription<T, E>
+    where
+        T: Event<E>,
+    {
+        let ctx = self.clone();
+        self.with_inner(move |inner| match inner.get_sub_receiver(&event) {
+            Some((rx, count)) => {
+                count.fetch_add(1, Ordering::Relaxed);
+                Subscription::new(rx.reactivate(), count, ctx)
+            },
+            None => {
+                let (emitter, rx) = subscription::channel();
+                let sub_ctx = event.subscribe(emitter, &ctx);
+                let count = Arc::new(AtomicU32::new(1));
+                inner.insert_subscription_state(
+                    Arc::clone(&count),
+                    event,
+                    rx.clone().deactivate().into_any(),
+                    sub_ctx,
+                );
+                Subscription::new(rx, count, ctx)
+            },
+        })
+    }
+
+    #[inline]
+    fn with_inner<R, F: FnOnce(&mut ContextInner<E>) -> R>(&self, f: F) -> R {
+        todo!();
+    }
 }
 
 impl<E> Clone for Context<E> {
@@ -8,4 +50,98 @@ impl<E> Clone for Context<E> {
     fn clone(&self) -> Self {
         todo!();
     }
+}
+
+struct ContextInner<E> {
+    editor: E,
+
+    /// Map from the `TypeId` of a given event to a list of active
+    /// subscriptions, sorted according to the event's `Ord` impl.
+    subscriptions: HashMap<TypeId, Vec<SubscriptionState>>,
+}
+
+impl<E: Editor> ContextInner<E> {
+    /// Returns the receiver for the givent event, or `None` if there aren't
+    /// any active [`Subscription`]s for it.
+    #[inline]
+    fn get_sub_receiver<T: Event<E>>(
+        &self,
+        event: &T,
+    ) -> Option<(InactiveReceiver<T::Payload>, Arc<AtomicU32>)> {
+        let vec = self.subscriptions.get(&TypeId::of::<T>())?;
+
+        let idx = vec
+            .binary_search_by(|subscription| {
+                // SAFETY: todo.
+                //
+                // TODO: use `downcast_ref_unchecked` once it's stable.
+                let probe = unsafe {
+                    subscription
+                        .event
+                        .as_ref()
+                        .downcast_ref::<T>()
+                        .unwrap_unchecked()
+                };
+
+                probe.cmp(event)
+            })
+            .ok()?;
+
+        // SAFETY: todo.
+        let inactive_rx = unsafe { vec[idx].rx.downcast_ref_unchecked() };
+        let count = &vec[idx].active_rx_count;
+        Some((inactive_rx.clone(), Arc::clone(count)))
+    }
+
+    /// TODO: docs.
+    #[inline]
+    fn insert_subscription_state<T: Event<E>>(
+        &mut self,
+        active_rx_count: Arc<AtomicU32>,
+        event: T,
+        rx: AnyReceiver,
+        sub_ctx: T::SubscribeCtx,
+    ) {
+        let vec = self.subscriptions.entry(TypeId::of::<T>()).or_default();
+
+        let Err(idx) = vec.binary_search_by(|subscription| {
+            // SAFETY: todo.
+            //
+            // TODO: use `downcast_ref_unchecked` once it's stable.
+            let probe = unsafe {
+                subscription
+                    .event
+                    .as_ref()
+                    .downcast_ref::<T>()
+                    .unwrap_unchecked()
+            };
+
+            probe.cmp(&event)
+        }) else {
+            panic!("event already has a subscription");
+        };
+
+        let state = SubscriptionState {
+            active_rx_count,
+            event: Box::new(event),
+            rx,
+            sub_ctx: Box::new(sub_ctx),
+        };
+
+        vec.insert(idx, state);
+    }
+}
+
+struct SubscriptionState {
+    /// .
+    active_rx_count: Arc<AtomicU32>,
+
+    /// .
+    event: Box<dyn Any>,
+
+    /// A type-erased, inactive receiver for payloads of a given event.
+    rx: AnyReceiver,
+
+    /// .
+    sub_ctx: Box<dyn Any>,
 }
--- crates/nomad2/src/subscription.rs
+++ crates/nomad2/src/subscription.rs
@@ -1,5 +1,7 @@
 use core::pin::Pin;
 use core::task::{Context, Poll};
+use std::sync::atomic::AtomicU32;
+use std::sync::Arc;
 
 use futures_util::Stream;
 
@@ -7,8 +9,19 @@ use crate::{Editor, Event};
 
 /// TODO: docs.
 pub struct Subscription<T: Event<E>, E: Editor> {
-    event: T,
-    editor: E,
+    rx: Receiver<T::Payload>,
+    count: Arc<AtomicU32>,
+    ctx: crate::Context<E>,
+}
+
+impl<T: Event<E>, E: Editor> Subscription<T, E> {
+    pub(crate) fn new(
+        rx: Receiver<T::Payload>,
+        count: Arc<AtomicU32>,
+        ctx: crate::Context<E>,
+    ) -> Self {
+        Self { rx, count, ctx }
+    }
 }
 
 impl<T: Event<E>, E: Editor> Stream for Subscription<T, E> {
@@ -19,6 +32,73 @@ impl<T: Event<E>, E: Editor> Stream for Subscription<T, E> {
         self: Pin<&mut Self>,
         _ctx: &mut Context<'_>,
     ) -> Poll<Option<Self::Item>> {
+        todo!();
+    }
+}
+
+pub(crate) fn channel<T>() -> (Emitter<T>, Receiver<T>) {
+    todo!();
+}
+
+/// TODO: docs.
+pub struct Emitter<T> {
+    item: T,
+}
+
+impl<T> Emitter<T> {
+    /// TODO: docs.
+    #[inline]
+    pub fn send(&self, _: T) {
+        todo!();
+    }
+}
+
+pub(crate) struct Receiver<T> {
+    inner: T,
+}
+
+impl<T> Receiver<T> {
+    pub(crate) fn deactivate(self) -> InactiveReceiver<T> {
+        todo!();
+    }
+}
+
+impl<T> Clone for Receiver<T> {
+    #[inline]
+    fn clone(&self) -> Self {
+        todo!();
+    }
+}
+
+pub(crate) struct InactiveReceiver<T> {
+    inner: T,
+}
+
+impl<T> InactiveReceiver<T> {
+    pub(crate) fn reactivate(self) -> Receiver<T> {
+        todo!();
+    }
+
+    pub(crate) fn into_any(self) -> AnyReceiver {
+        todo!();
+    }
+}
+
+impl<T> Clone for InactiveReceiver<T> {
+    #[inline]
+    fn clone(&self) -> Self {
+        todo!();
+    }
+}
+
+pub(crate) struct AnyReceiver {
+    inner: InactiveReceiver<()>,
+}
+
+impl AnyReceiver {
+    pub(crate) unsafe fn downcast_ref_unchecked<T>(
+        &self,
+    ) -> &InactiveReceiver<T> {
         todo!();
     }
 }
