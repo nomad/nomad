acc143b973027d923f2ceee2c9bd874c35df2fd4
Riccardo Mazzarini <me@noib3.dev>
2024-10-07 17:14:48 +0800

    feat(neovim): add `CloseBuffer{Event}`

--- crates/nomad/src/neovim/events/mod.rs
+++ crates/nomad/src/neovim/events/mod.rs
@@ -1,7 +1,9 @@
 //! TODO: docs.
 
+mod close_buffer;
 mod focus_buffer;
 mod open_buffer;
 
+pub use close_buffer::{CloseBuffer, CloseBufferEvent};
 pub use focus_buffer::{FocusBuffer, FocusBufferEvent};
 pub use open_buffer::{OpenBuffer, OpenBufferEvent};
