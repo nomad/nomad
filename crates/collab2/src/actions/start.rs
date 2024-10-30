bfc1336881e0e9f5c2f2984fea974644abeb66da
Riccardo Mazzarini <me@noib3.dev>
2024-10-30 15:46:43 +0800

    collab2: sketch `AsyncAction` for `Start`

--- crates/collab2/src/actions/start.rs
+++ crates/collab2/src/actions/start.rs
@@ -1,6 +1,10 @@
+use collab_server::message::Message;
+use futures_util::StreamExt;
 use nomad::ctx::NeovimCtx;
-use nomad::{action_name, Action, ActionName, Shared};
+use nomad::{action_name, ActionName, AsyncAction, Shared};
 
+use super::UserBusyError;
+use crate::session::Session;
 use crate::session_status::SessionStatus;
 use crate::Collab;
 
@@ -15,15 +19,34 @@ impl Start {
     }
 }
 
-impl<'a> Action<NeovimCtx<'a>> for Start {
+impl AsyncAction for Start {
     const NAME: ActionName = action_name!("start");
     type Args = ();
     type Docs = ();
     type Module = Collab;
-    type Return = ();
 
-    fn execute(&mut self, _args: Self::Args, _: NeovimCtx<'a>) {
-        todo!()
+    async fn execute(
+        &mut self,
+        _: Self::Args,
+        ctx: NeovimCtx<'_>,
+    ) -> Result<(), UserBusyError> {
+        match self.session_status.with(|s| UserBusyError::try_from(s)).ok() {
+            Some(err) => return Err(err),
+            _ => self.session_status.set(SessionStatus::Starting),
+        }
+
+        let mut session = Session::start().await;
+        self.session_status.set(SessionStatus::InSession(session.project()));
+        ctx.spawn(async move {
+            let (tx, rx) = flume::unbounded::<Message>();
+            let tx = tx.into_sink::<'static>();
+            let rx = rx
+                .into_stream::<'static>()
+                .map(Ok::<_, core::convert::Infallible>);
+            let _err = session.run(tx, rx).await;
+        });
+
+        Ok(())
     }
 
     fn docs(&self) -> Self::Docs {}
