f6aab248fc3a796543d5d091f5425f8204647813
Riccardo Mazzarini <me@noib3.dev>
2025-01-25 01:55:19 +0800

    Implement `FindRootArgs::find()`

--- crates/collab2/src/collab_backend.rs
+++ crates/collab2/src/collab_backend.rs
@@ -35,12 +35,16 @@ mod neovim {
 
     use super::*;
 
+    const MARKERS: Markers = root_markers::GitDirectory;
+
+    pub type Markers = root_markers::GitDirectory;
+
     pub enum NeovimFindProjectRootError {
         LspRootDirNotAbsolute(fs::AbsPathNotAbsoluteError),
         CouldntFindRoot,
         HomeDir(NeovimHomeDirError),
         InvalidBufferPath(String),
-        MarkedRoot(root_markers::FindRootError<NeovimFs>),
+        MarkedRoot(root_markers::FindRootError<NeovimFs, Markers>),
         IsParentDir(<NeovimFs as Fs>::NodeAtPathError),
     }
 
@@ -77,7 +81,7 @@ mod neovim {
                 .map_err(NeovimFindProjectRootError::HomeDir)?;
 
             let args = root_markers::FindRootArgs {
-                marker: root_markers::GitDirectory,
+                marker: MARKERS,
                 start_from: &buffer_path,
                 stop_at: Some(&home_dir),
             };
@@ -156,9 +160,11 @@ mod neovim {
 
 #[cfg(feature = "neovim")]
 mod root_markers {
+    use futures_util::stream::{self, StreamExt};
+    use futures_util::{pin_mut, select};
     use nvimx2::fs::{self, DirEntry};
 
-    pub(super) struct FindRootArgs<'a, M> {
+    pub struct FindRootArgs<'a, M> {
         /// The marker used to determine if a directory is the root.
         pub(super) marker: M,
 
@@ -174,20 +180,9 @@ mod root_markers {
         pub(super) stop_at: Option<&'a fs::AbsPath>,
     }
 
-    impl<M> FindRootArgs<'_, M> {
-        pub(super) async fn find<Fs>(
-            self,
-            fs: &mut Fs,
-        ) -> Result<Option<fs::AbsPathBuf>, FindRootError<Fs>>
-        where
-            Fs: fs::Fs,
-            M: RootMarker<Fs>,
-        {
-            todo!();
-        }
-    }
+    pub struct GitDirectory;
 
-    pub(super) trait RootMarker<Fs: fs::Fs> {
+    pub trait RootMarker<Fs: fs::Fs> {
         type Error;
 
         fn matches(
@@ -196,25 +191,7 @@ mod root_markers {
         ) -> impl Future<Output = Result<bool, Self::Error>>;
     }
 
-    pub(super) struct GitDirectory;
-
-    impl<Fs: fs::Fs> RootMarker<Fs> for GitDirectory {
-        type Error = DirEntryError<Fs>;
-
-        async fn matches(
-            &self,
-            dir_entry: &Fs::DirEntry,
-        ) -> Result<bool, Self::Error> {
-            Ok(dir_entry.name().await.map_err(DirEntryError::Name)?.as_ref()
-                == ".git"
-                && dir_entry
-                    .is_directory()
-                    .await
-                    .map_err(DirEntryError::NodeKind)?)
-        }
-    }
-
-    pub(super) enum FindRootError<Fs: fs::Fs> {
+    pub enum FindRootError<Fs: fs::Fs, M: RootMarker<Fs>> {
         /// TODO: docs.
         DirEntry {
             /// TODO: docs.
@@ -222,6 +199,9 @@ mod root_markers {
             /// TODO: docs.
             err: DirEntryError<Fs>,
         },
+
+        /// TODO: docs.
+        Marker(M::Error),
 
         /// TODO: docs.
         NodeAtStartPath(Fs::NodeAtPathError),
@@ -238,9 +218,119 @@ mod root_markers {
         StartPathNotFound,
     }
 
-    pub(super) enum DirEntryError<Fs: fs::Fs> {
+    pub enum DirEntryError<Fs: fs::Fs> {
         Access(Fs::DirEntryError),
         Name(<Fs::DirEntry as fs::DirEntry>::NameError),
         NodeKind(<Fs::DirEntry as fs::DirEntry>::NodeKindError),
+    }
+
+    impl<M> FindRootArgs<'_, M> {
+        pub(super) async fn find<Fs>(
+            self,
+            fs: &mut Fs,
+        ) -> Result<Option<fs::AbsPathBuf>, FindRootError<Fs, M>>
+        where
+            Fs: fs::Fs,
+            M: RootMarker<Fs>,
+        {
+            let node_kind = fs
+                .node_at_path(self.start_from)
+                .await
+                .map_err(FindRootError::NodeAtStartPath)?
+                .ok_or(FindRootError::StartPathNotFound)?
+                .kind();
+
+            let mut dir = match node_kind {
+                fs::FsNodeKind::Directory => self.start_from,
+                fs::FsNodeKind::File => self
+                    .start_from
+                    .parent()
+                    .expect("path is of file, so it must have a parent"),
+                fs::FsNodeKind::Symlink => todo!("can't handle symlinks yet"),
+            }
+            .to_owned();
+
+            loop {
+                if self.contains_marker(&dir, fs).await? {
+                    return Ok(Some(dir));
+                }
+                if self.stop_at == Some(&*dir) {
+                    return Ok(None);
+                }
+                if !dir.pop() {
+                    return Ok(None);
+                }
+            }
+        }
+
+        async fn contains_marker<Fs>(
+            &self,
+            dir_path: &fs::AbsPath,
+            fs: &mut Fs,
+        ) -> Result<bool, FindRootError<Fs, M>>
+        where
+            Fs: fs::Fs,
+            M: RootMarker<Fs>,
+        {
+            let read_dir = fs
+                .read_dir(dir_path)
+                .await
+                .map_err(|err| FindRootError::ReadDir {
+                    dir_path: dir_path.to_owned(),
+                    err,
+                })?
+                .fuse();
+
+            pin_mut!(read_dir);
+
+            let mut check_marker_matches = stream::FuturesUnordered::new();
+
+            loop {
+                select! {
+                    read_res = read_dir.select_next_some() => {
+                        let dir_entry =
+                            read_res.map_err(|err| FindRootError::DirEntry {
+                                path: dir_path.to_owned(),
+                                err: DirEntryError::Access(err),
+                            })?;
+
+                        let fut = async move {
+                            self.marker
+                                .matches(&dir_entry)
+                                .await
+                                .map_err(FindRootError::Marker)
+                        };
+
+                        check_marker_matches.push(fut);
+                    },
+
+                    marker_res = check_marker_matches.select_next_some() => {
+                        match marker_res {
+                            Ok(true) => return Ok(true),
+                            Ok(false) => continue,
+                            Err(err) => return Err(err),
+                        }
+                    },
+
+                    complete => return Ok(false),
+                }
+            }
+        }
+    }
+
+    impl<Fs: fs::Fs> RootMarker<Fs> for GitDirectory {
+        type Error = DirEntryError<Fs>;
+
+        async fn matches(
+            &self,
+            dir_entry: &Fs::DirEntry,
+        ) -> Result<bool, Self::Error> {
+            Ok(dir_entry.name().await.map_err(DirEntryError::Name)?.as_ref()
+                == ".git"
+                && dir_entry
+                    .is_directory()
+                    .await
+                    .map_err(DirEntryError::NodeKind)?)
+        }
     }
 }
