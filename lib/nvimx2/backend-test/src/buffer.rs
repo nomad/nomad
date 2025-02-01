9d832cf63d710581f78918430dd8b13498358258
Riccardo Mazzarini <me@noib3.dev>
2025-02-01 17:46:53 +0800

    Implement `Buffer<TestBackend>` for `TestBuffer`

--- lib/nvimx2/backend-test/src/buffer.rs
+++ lib/nvimx2/backend-test/src/buffer.rs
@@ -1,6 +1,32 @@
+use std::borrow::Cow;
+
+use crop::Rope;
+use nvimx_core::ByteOffset;
+use nvimx_core::backend::Buffer;
+
+use crate::TestBackend;
+
 /// TODO: docs.
-pub struct TestBuffer;
+pub struct TestBuffer {
+    contents: Rope,
+    id: TestBufferId,
+    name: String,
+}
 
 /// TODO: docs.
 #[derive(Debug, Copy, Clone)]
 pub struct TestBufferId;
+
+impl Buffer<TestBackend> for TestBuffer {
+    fn byte_len(&self) -> ByteOffset {
+        self.contents.byte_len().into()
+    }
+
+    fn id(&self) -> TestBufferId {
+        self.id
+    }
+
+    fn name(&self) -> Cow<'_, str> {
+        Cow::Borrowed(&self.name)
+    }
+}
