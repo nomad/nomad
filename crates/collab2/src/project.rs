d47f9ea5bf3362ba513bbe58ddb55147ad5ec901
Riccardo Mazzarini <me@noib3.dev>
2025-03-02 21:24:44 +0100

    WIP

--- crates/collab2/src/project.rs
+++ crates/collab2/src/project.rs
@@ -1,10 +1,11 @@
 use core::marker::PhantomData;
+use std::collections::hash_map::Entry;
 
 use collab_server::SessionId;
 use collab_server::message::{Peer, Peers};
 use eerie::Replica;
 use fxhash::FxHashMap;
-use nvimx2::fs::{AbsPathBuf, Fs};
+use nvimx2::fs::{AbsPath, AbsPathBuf, Fs};
 use nvimx2::{AsyncCtx, Shared, notify};
 use smol_str::ToSmolStr;
 
@@ -12,9 +13,12 @@ use crate::CollabBackend;
 
 /// TODO: docs.
 pub struct Project<B: CollabBackend> {
+    host: Peer,
+    local_peer: Peer,
     projects: Projects<B>,
-    _replica: Replica,
-    _root: AbsPathBuf,
+    remote_peers: Peers,
+    replica: Replica,
+    root: AbsPathBuf,
     session_id: SessionId,
 }
 
@@ -52,6 +56,11 @@ enum ProjectState<B: CollabBackend> {
 
 impl<B: CollabBackend> Project<B> {
     /// TODO: docs.
+    pub fn is_host(&self) -> bool {
+        self.local_peer.id() == self.host.id()
+    }
+
+    /// TODO: docs.
     pub(crate) async fn flush(
         &self,
         _project_root: &<B::Fs as Fs>::Directory,
@@ -65,25 +74,31 @@ impl<B: CollabBackend> Projects<B> {
         &self,
         session_id: SessionId,
     ) -> Option<Shared<Project<B>>> {
-        self.map.with(|map| map.get(&session_id).cloned())
+        self.map.with(|map| match map.get(&session_id)? {
+            ProjectState::Active(project) => Some(project.clone()),
+            _ => None,
+        })
     }
 
-    pub(crate) fn insert(
+    pub(crate) fn join_guard(
         &self,
-        args: NewProjectArgs,
-    ) -> Result<Shared<Project<B>>, OverlappingProjectError> {
-        let project = Shared::new(Project {
+        project_root: AbsPathBuf,
+        session_id: SessionId,
+    ) -> Result<ProjectGuard<B>, OverlappingProjectError> {
+        let guard = ProjectGuard {
+            root: project_root,
             projects: self.clone(),
-            _replica: args.replica,
-            _root: args.root,
-            session_id: args.session_id,
-        });
+            session_id,
+        };
+
+        let state = ProjectState::Joining(guard.priv_clone());
 
         self.map.with_mut(|map| {
-            map.insert(args.session_id, project.clone());
+            let prev = map.insert(session_id, state);
+            assert!(prev.is_none());
         });
 
-        Ok(project)
+        Ok(guard)
     }
 
     pub(crate) async fn select(
@@ -119,6 +134,38 @@ impl<B: CollabBackend> Projects<B> {
     }
 }
 
+impl<B: CollabBackend> ProjectGuard<B> {
+    pub(crate) fn activate(self, args: NewProjectArgs) -> Shared<Project<B>> {
+        let project = Shared::new(Project {
+            host: args.host,
+            local_peer: args.local_peer,
+            projects: self.projects.clone(),
+            remote_peers: args.remote_peers,
+            replica: args.replica,
+            root: self.root.clone(),
+            session_id: self.session_id,
+        });
+
+        self.projects.map.with_mut(|map| {
+            map.insert(self.session_id, ProjectState::Active(project.clone()));
+        });
+
+        project
+    }
+
+    pub(crate) fn root(&self) -> &AbsPath {
+        &self.root
+    }
+
+    fn priv_clone(&self) -> Self {
+        Self {
+            root: self.root.clone(),
+            projects: self.projects.clone(),
+            session_id: self.session_id,
+        }
+    }
+}
+
 impl<B> NoActiveSessionError<B> {
     pub(crate) fn new() -> Self {
         Self(PhantomData)
@@ -142,6 +189,23 @@ impl<B: CollabBackend> Default for Projects<B> {
 impl<B: CollabBackend> Clone for Projects<B> {
     fn clone(&self) -> Self {
         Self { map: self.map.clone() }
+    }
+}
+
+impl<B: CollabBackend> Drop for ProjectGuard<B> {
+    fn drop(&mut self) {
+        self.projects.map.with_mut(|map| {
+            let Entry::Occupied(entry) = map.entry(self.session_id) else {
+                unreachable!()
+            };
+
+            if matches!(
+                entry.get(),
+                ProjectState::Joining(_) | ProjectState::Starting(_)
+            ) {
+                entry.remove();
+            }
+        });
     }
 }
 
