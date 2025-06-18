561c1206da6cdcdcc5fc923cad6a9b1b13226d95
Riccardo Mazzarini <me@noib3.dev>
2025-06-18 18:54:02 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -56,6 +56,7 @@
                     "cargo"
                     "clippy"
                     "rust-src"
+                    "rust-std"
                     "rustc"
                     "rustfmt"
                   ])
