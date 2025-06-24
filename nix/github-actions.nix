bc8552b472f48ba6fe2ef033da66eda7565bfe48
Riccardo Mazzarini <me@noib3.dev>
2025-06-24 19:02:52 +0200

    Create minimal `gha-tests` devShell to run tests in

--- nix/github-actions.nix
+++ nix/github-actions.nix
@@ -5,13 +5,38 @@
 {
   perSystem =
     {
+      pkgs,
+      lib,
       inputs',
+      crane,
       ...
     }:
+    let
+      devShells = {
+        tests = {
+          packages = with crane; [
+            cargo
+            rustc
+          ];
+        };
+      };
+
+      mkDevShell =
+        devShell:
+        pkgs.mkShell {
+          buildInputs = (crane.commonArgs.buildInputs or [ ]) ++ (devShell.buildInputs or [ ]);
+          nativeBuildInputs = (crane.commonArgs.nativeBuildInputs or [ ]) ++ (devShell.packages or [ ]);
+        };
+    in
     {
       apps.nix-develop-gha = {
         type = "app";
         program = "${inputs'.nix-develop-gha.packages.default}/bin/nix-develop-gha";
       };
+
+      devShells = lib.mapAttrs' (name: devShell: {
+        name = "gha-${name}";
+        value = mkDevShell devShell;
+      }) devShells;
     };
 }
