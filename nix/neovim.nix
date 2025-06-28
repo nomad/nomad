34a2c0be344a4af247fca58275775ab65dfa1970
Riccardo Mazzarini <me@noib3.dev>
2025-06-28 12:03:01 +0200

    refactor: build release artifacts via Nix

--- nix/neovim.nix
+++ nix/neovim.nix
@@ -124,6 +124,25 @@
       packages = {
         neovim = mkPlugin { isNightly = false; };
         neovim-nightly = mkPlugin { isNightly = true; };
+        neovim-release-artifacts = builtins.derivation {
+          # - os: ubuntu-latest
+          #   targets: "aarch64-unknown-linux-gnu,x86_64-unknown-linux-gnu"
+          # - os: macos-latest
+          #   targets: "aarch64-apple-darwin,x86_64-apple-darwin"
+          #
+          #
+          # mkdir -p build
+          #
+          # IFS=',' read -ra TARGETS <<< "${{ matrix.targets }}"
+          #
+          # for neovim in "0.11" "nightly"; do
+          #   for target in "${TARGETS[@]}"; do
+          #     nightly_flag=$([ "$neovim" = "nightly" ] && echo "--nightly" || echo "")
+          #     cargo xtask build $nightly_flag -- --release --target $target
+          #     tar -czf "build/mad-neovim-$neovim-$target.tar.gz" lua
+          #   done
+          # done
+        };
       };
     };
 }
