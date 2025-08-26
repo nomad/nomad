c8ff643d267b87d7145fb9a502e2e02679448246
Riccardo Mazzarini <me@noib3.dev>
2025-08-26 11:34:52 +0200

    tests(collab): add failing test

--- tests/src/collab/event_stream.rs
+++ tests/src/collab/event_stream.rs
@@ -1,4 +1,4 @@
-use abs_path::{AbsPath, path};
+use abs_path::{AbsPath, AbsPathBuf, path};
 use collab::event::{BufferEvent, Event, EventStream};
 use collab::mock::CollabMock;
 use collab::{CollabEditor, PeerId};
@@ -27,8 +27,31 @@ fn editing_watched_buffer_emits_event() {
         });
 
         let (buffer_id, replacements) = event_stream.next_as_edit(ctx).await;
+
         assert_eq!(buffer_id, foo_id);
         assert_eq!(replacements.as_ref(), [Replacement::insertion(5, ", ")]);
+    });
+}
+
+#[test]
+fn creating_buffer_emits_event() {
+    let fs = mock::fs! {
+        "foo.txt": "hello world",
+    };
+
+    CollabMock::new(Mock::new(fs)).block_on(async |ctx| {
+        let agent_id = ctx.new_agent_id();
+
+        let mut event_stream = EventStream::new(path!("/"), ctx).await;
+
+        let foo_id =
+            ctx.create_buffer(path!("/foo.txt"), agent_id).await.unwrap();
+
+        let (buffer_id, file_path) =
+            event_stream.next_as_buffer_created(ctx).await;
+
+        assert_eq!(buffer_id, foo_id);
+        assert_eq!(file_path, path!("/foo.txt"));
     });
 }
 
@@ -64,6 +87,20 @@ trait EventStreamExt<Ed: CollabEditor> {
                 Ok(Event::Buffer(event)) => event,
                 Ok(other) => panic!("expected BufferEvent, got {other:?}"),
                 Err(err) => panic!("{err}"),
+            }
+        }
+    }
+
+    fn next_as_buffer_created(
+        &mut self,
+        ctx: &mut Context<Ed>,
+    ) -> impl Future<Output = (Ed::BufferId, AbsPathBuf)> {
+        async move {
+            match self.next_as_buffer(ctx).await {
+                BufferEvent::Created(buffer_id, file_path) => {
+                    (buffer_id, file_path)
+                },
+                other => panic!("expected Created event, got {other:?}"),
             }
         }
     }
