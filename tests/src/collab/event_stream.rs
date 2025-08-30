48f39c9e31ab3d843ec74d74810c2fe1dc7bf46b
Riccardo Mazzarini <me@noib3.dev>
2025-08-30 11:26:38 +0200

    test(collab): add failing test

--- tests/src/collab/event_stream.rs
+++ tests/src/collab/event_stream.rs
@@ -1,3 +1,5 @@
+use core::time::Duration;
+
 use abs_path::{AbsPath, AbsPathBuf, path};
 use collab::event::{
     BufferEvent,
@@ -10,6 +12,8 @@ use collab::mock::CollabMock;
 use collab::{CollabEditor, PeerId};
 use editor::{Buffer, ByteOffset, Context, Cursor, Replacement};
 use mock::{EditorExt, Mock};
+
+use crate::utils::FutureExt;
 
 #[test]
 fn editing_watched_buffer_emits_event() {
@@ -89,6 +93,55 @@ fn moving_cursor_emits_event() {
 
         assert_eq!(cursor_id, foo_cursor_id);
         assert_eq!(new_offset, 6);
+    });
+}
+
+#[test]
+fn moving_cursor_immediately_after_creating_it_should_converge() {
+    let fs = mock::fs! {
+        "foo.txt": "hello world",
+    };
+
+    CollabMock::new(Mock::new(fs)).block_on(async |ctx| {
+        let agent_id = ctx.new_agent_id();
+
+        let foo_id =
+            ctx.create_buffer(path!("/foo.txt"), agent_id).await.unwrap();
+
+        let mut event_stream = EventStream::new(path!("/"), ctx).await;
+
+        let initial_offset = 5;
+
+        let final_offset = 6;
+
+        // Create a cursor and immediately move it to a different offset before
+        // the EventStream gets to handle the CursorCreated event.
+        ctx.with_borrowed(|ctx| {
+            let _ = ctx
+                .buffer(foo_id)
+                .unwrap()
+                .create_cursor(initial_offset, agent_id)
+                .schedule_move(final_offset, agent_id);
+        });
+
+        let mut observed_offset = initial_offset;
+
+        // Drain the event stream, updating the observed offset.
+        while let Some(cursor_event) = event_stream
+            .next_as_cursor(ctx)
+            .timeout(Duration::from_millis(500))
+            .await
+        {
+            observed_offset = match cursor_event.kind {
+                CursorEventKind::Created(_, offset) => offset,
+                CursorEventKind::Moved(offset) => offset,
+                CursorEventKind::Removed => unreachable!(),
+            }
+        }
+
+        // The observed cursor offset after all events have been emitted
+        // and handled should still be the final offset.
+        assert_eq!(observed_offset, final_offset);
     });
 }
 
