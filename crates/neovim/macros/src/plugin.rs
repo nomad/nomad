8328bb7954d06e9b6d68b8d6e612260506331f5f
Riccardo Mazzarini <me@noib3.dev>
2025-09-02 12:52:33 +0200

    fix(neovim): handle plugins being `require`d multiple times

--- crates/neovim/macros/src/plugin.rs
+++ crates/neovim/macros/src/plugin.rs
@@ -8,14 +8,22 @@ pub(crate) fn plugin(_attr: TokenStream, item: TokenStream) -> TokenStream {
 
     let fun_name = &fun.sig.ident;
     let fun_body = &fun.block;
-    let augroup_name = fun_name.to_string();
+    let plugin_name = fun_name.to_string();
 
     quote! {
         #[::neovim::oxi::plugin(nvim_oxi = ::neovim::oxi)]
         fn #fun_name() -> ::neovim::oxi::Dictionary {
-            let plugin = #fun_body;
-            let neovim = ::neovim::Neovim::new_plugin(#augroup_name);
-            ::editor::module::Plugin::api(plugin, neovim).into()
+            // Guard against the user resetting package.loaded.<plugin> to nil
+            // after 'require'ing the plugin for the first time.
+            ::std::thread_local! {
+                static API: ::core::cell::LazyCell::<::neovim::oxi::Dictionary>
+                    = ::core::cell::LazyCell::new(|| {
+                        let plugin = #fun_body;
+                        let neovim = ::neovim::Neovim::new_plugin(#plugin_name);
+                        ::editor::module::Plugin::api(plugin, neovim).into()
+                    });
+            };
+            API.with(|api| ::core::cell::LazyCell::force(api).clone())
         }
     }
     .into()
